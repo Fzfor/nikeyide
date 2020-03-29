5825dd8a9f448a79892ddeabf7e1c3c8b9b89c4c
Fzfor <346224813@qq.com>
2020-03-29 23:08:18 +0800

    20200329

--- src/main/java/com/jiayou/leetcode/AddTwoNumbers.java
+++ src/main/java/com/jiayou/leetcode/AddTwoNumbers.java
@@ -4,6 +4,32 @@ import java.util.List;
 
 /**
  * Create by fz on 2020/3/28
+ * You are given two non-empty linked lists representing two non-negative integers. The digits are stored in reverse order and each of their nodes contain a single digit. Add the two numbers and return it as a linked list.
+ * <p>
+ * You may assume the two numbers do not contain any leading zero, except the number 0 itself.
+ * <p>
+ * Example:
+ * <p>
+ * Input: (2 -> 4 -> 3) + (5 -> 6 -> 4)
+ * Output: 7 -> 0 -> 8
+ * Explanation: 342 + 465 = 807.
+ * <p>
+ * <p>
+ * 给出两个 非空 的链表用来表示两个非负的整数。其中，它们各自的位数是按照 逆序 的方式存储的，并且它们的每个节点只能存储 一位 数字。
+ * <p>
+ * 如果，我们将这两个数相加起来，则会返回一个新的链表来表示它们的和。
+ * <p>
+ * 您可以假设除了数字 0 之外，这两个数都不会以 0 开头。
+ * <p>
+ * 示例：
+ * <p>
+ * 输入：(2 -> 4 -> 3) + (5 -> 6 -> 4)
+ * 输出：7 -> 0 -> 8
+ * 原因：342 + 465 = 807
+ * <p>
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/add-two-numbers
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
  */
 public class AddTwoNumbers {
     public static void main(String[] args) {
@@ -36,58 +62,39 @@ public class AddTwoNumbers {
     }
 
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-        long a = 0;
-        long b = 0;
-        long index1 = -1;
-        long index2 = -1;
+        if (l1 == null && l2 == null) return null;
 
-        while (true) {
-            index1++;
-            double power =  Math.pow(10, index1); //10 ^ index1 ;
-            a += l1.val * power;
-            if (l1.next == null) {
-                break;
-            } else {
+        ListNode dummyNode = new ListNode(0);
+
+        ListNode head = dummyNode;
+        int a = 0, b = 0, sum = 0;
+        //创建进位数
+        int carry = 0;
+        while (l1 != null || l2 != null || carry != 0) {
+            a = l1 == null ? 0 : l1.val;
+            b = l2 == null ? 0 : l2.val;
+
+            sum = a + b + carry;
+
+            //将和的个位数放到链表中
+            head.next = new ListNode(sum % 10);
+            //将指针后移
+            head = head.next;
+
+            //更新进位
+            carry = sum / 10;
+
+            //将l1 l2链表指针后移
+            if (l1 != null) {
                 l1 = l1.next;
             }
-        }
-        while (true) {
-            index2++;
-            double power =  Math.pow(10, index2);//10 ^ index2;
-            b += l2.val * power;
-            if (l2.next == null) {
-                break;
-            } else {
+            if (l2 != null) {
                 l2 = l2.next;
             }
+
+
         }
-
-        double result = a + b;
-
-        double result1 = result;
-        //判断和为几位数
-        int unit = 1;
-        while (true) {
-            if (result1 >= 10) {
-                unit++;
-                result1 = result1 / 10;
-            } else {
-                break;
-            }
-        }
-
-        //创建一个数组，填充结果的各个位上的数字
-        ListNode[] vals = new ListNode[unit];
-        for (int i = 0; i < unit; i++) {
-            int value =  (int)(result %  Math.pow(10, i + 1) /  Math.pow(10, i)); //result % 10 ^ (i + 1) / 10 ^ i;
-            vals[i] = new ListNode(value);
-        }
-
-        for (int i = 0; i < vals.length - 1; i++) {
-            vals[i].next = vals[i + 1];
-        }
-
-        return vals[0];
+        return dummyNode.next;
 
     }
 }
